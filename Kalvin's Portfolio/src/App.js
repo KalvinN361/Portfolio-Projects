9622f6b2b8f82f61a423b1f518b7c43c3113cd74
Kalvin <kalvin.win59@gmail.com>
2022-12-07 14:54:40 -0600

    updated my story

--- Kalvin's Portfolio/src/App.js
+++ Kalvin's Portfolio/src/App.js
@@ -5,21 +5,21 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-      <Portfolio />
-      <Experience />
-      <Contact />
-      <Footer />
-      <SocialLinks />
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			<Home />
+			<About />
+			<Portfolio />
+			<Experience />
+			<Contact />
+			<Footer />
+			<SocialLinks />
+		</div>
+	);
 }
 
 export default App;
--- Kalvin's Portfolio/src/components/About.jsx
+++ Kalvin's Portfolio/src/components/About.jsx
@@ -9,22 +9,23 @@ const About = () => {
 			<div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
 				<div className="pb-8">
 					<p className="text-4xl font-bold inline border-b-4 border-gray-500">
-						About
+						About Me
 					</p>
 				</div>
 
 				<p className="text-xl mt-5">
 					I am a software engineer, working in both, backend and frontend
-					programming. Excited for improving my skills and learning new
-					technologies.
+					programming with working knowledge of AWS. Excited for improving my
+					skills and learning new technologies.
 				</p>
 
 				<br />
 
 				<p className="text-xl">
 					I thrive in a team environment while also being able to work
-					individually <br /> <br />I found my love for coding in highschool,
-					but fell for it after college
+					individually <br /> <br />I began coding in highschool, but ultimately
+					went to school for dental. After College, I found my love for coding
+					again and decided to pursue it as a career.
 				</p>
 			</div>
 		</div>
